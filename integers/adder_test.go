63aa1a2dbcb6c2e70b01f3904e9f24dd56315f35
devxdh <devxdhanadiya@gmail.com>
2026-06-28 06:42:51 +0000

    (TDD): integers

---
+++ integers/adder_test.go
@@ -0,0 +1,26 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdded(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	assertCorrectMessage(t, sum, expected)
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func assertCorrectMessage(t testing.TB, sum, expected int) {
+	t.Helper()
+	if sum != expected {
+		t.Errorf("sum %q expected %q", sum, expected)
+	}
+}
